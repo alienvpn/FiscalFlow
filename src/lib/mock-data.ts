ada9ff4ed9c6734e3a2ac6783ac932609d03096a
MG Amila GAYAN <amila.millangoda@gmail.com>
2025-07-01 10:48:42 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/mock-data.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is no longer used and can be safely deleted.
-// All data is now managed in-memory via src/context/data-context.tsx
-// to allow for interactive data entry in the application.
